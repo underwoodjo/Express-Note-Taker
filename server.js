81bfa4334760ed018f8c1fdbe3513a3fdcb46fce
underwoodjo <132414633+underwoodjo@users.noreply.github.com>
2023-11-08 22:14:15 -0500

    edited gets, edited pathways

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const api = require
-
 const PORT = 3001;
 const app = express();
 
@@ -11,31 +9,31 @@ app.use(express.urlencoded({ extended: true }));
 // Middleware to serve up static assets from the public folder
 app.use(express.static('public'));
 
-// Send all the requests that begin with /api to the index.js in the routes folder
-app.use('/api', api);
-
 // Get Routes to the homepage
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, './public/index.html'))
 );
-
+// GET request for notes
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, './public/notes.html'))
 );
-// GET request for notes
+// GET request for api/notes
 app.get('/api/notes', (req, res) => {
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-        } else {
-            const parsedNotes = JSON.parse(data);
-        }
-    })
+  const file = './db/db.json';
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error:', err);
+      res.status(500).json({ error: 'Failed to read notes from the file.' });
+    } else {
+      const note = JSON.parse(data);
+      res.json(note);
+    }
   });
+});
 
   // POST request to add a note
 // app.post('/api/notes', (req, res) => {
-   
+
 
 // Listen
 app.listen(PORT, () =>
